b249ee036f059baaca7ab2e0e23c4b4fd1e030a8
dkonuk <58464780+dkonuk@users.noreply.github.com>
2024-06-02 23:20:36 +0300

    Update personelApp.java
    Departman listesi eklendi

--- test_automation_odev4/src/main/java/org/example/personel/personelApp.java
+++ test_automation_odev4/src/main/java/org/example/personel/personelApp.java
@@ -1,16 +1,21 @@
 package org.example.personel;
 
-import java.util.Date;
+import java.util.Arrays;
+
 
 public class personelApp {
     public static void main(String[] args) {
         personel personel001 = new personel();
+        String[] personelListesi = new String[2];
 
         personel001.isim = "Ahmet";
         personel001.soyisim = "Yazar";
         personel001.departman = "Güvenlik";
         personel001.pozisyon = "Şef";
         personel001.aktif_calisan= true;
+        personelListesi[0] = personel001.isim + " " + personel001.soyisim + "\n Departman: " + personel001.departman + " \nPozisyon: " + personel001.pozisyon + "\n " +"Halen Çalışıyor: " + personel001.aktif_calisan + "\n" ;
+
+
 
         personel personel002 = new personel();
 
@@ -19,10 +24,13 @@ public class personelApp {
         personel002.departman = "İnsan Kaynakları";
         personel002.pozisyon = "İşe alım Uzmanı" ;
         personel002.aktif_calisan = true;
+        personelListesi[1] = personel002.isim + " " + personel002.soyisim + "\n Departman " + personel002.departman + "\n Pozisyon " + personel002.pozisyon + "\n " + "Halen Çalışıyor: " + personel002.aktif_calisan;
 
 
         personel001.personel_bilgileri();
         personel002.personel_bilgileri();
+
+        System.out.println("Personel Listesi \n" + Arrays.toString(personelListesi));
 
     }
 }
